5cad3775d62a620e0356068c19471f9d8f7b6d01
WillianXRosa <wxrosa@hotmail.com>
2024-08-28 07:36:15 -0300

    28/08/2024  - Criacao do arquivo test application-test.properties e configuracao banco dados h2

--- src/main/java/com/profnelio/springbootcourse/entities/UserEntity.java
+++ src/main/java/com/profnelio/springbootcourse/entities/UserEntity.java
@@ -3,11 +3,23 @@ package com.profnelio.springbootcourse.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_user")
 public class UserEntity implements Serializable{	
 	
 	private static final long serialVersionUID = 1L;
 	
 	/**Atributos**/
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
